c0362e576513606ec7839585e137882a74227bc9
Daniel Medeiros <danielproenca@alunos.utfpr.edu.br>
2020-09-25 15:37:07 -0300

    Utilizado as bibliotecas java.sql e java.util

--- src/main/java/br/edu/utfpr/dv/sireata/dao/AnexoMetodos/MetreadDAO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/dao/AnexoMetodos/MetreadDAO.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 
 public class MetreadDAO {
 
